c038f8d9169f98b9f75372dcdaa6f6e909b41b3f
Nicolas Léveillé <nicolas@uucidl.com>
2013-12-10 23:33:35 +0100

    first failing unit test!

---
+++ test/core.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*global describe:false,it:false*/
+
+var assert = require('assert');
+
+describe('core', function () {
+    it('must respond', function (done) {
+        assert.ok(false);
+        done();
+    });
+});
